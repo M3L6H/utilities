cccdcd7eda2acca43f7c6cdeaa25fc667c328b8c
M3L6H <sisoac@hotmail.com>
2021-06-11 13:08:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Utilities
+
+A collection of utilities I have personally found helpful in my development
+career.
---
+++ gacp/package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+app="$(cat .app)"
+base_dir="$(dirname "$0")"
+dist="${base_dir}/dist"
+
+mkdir "$dist" >/dev/null 2>&1
+rm "$dist"/*.tar.gz
+tar -cvf "${dist}/${app}-$(<"${base_dir}/data/version").tar.gz" \
+  data/ \
+  scripts/ \
+  .app \
+  install.sh \
+  reinstall.sh \
+  uninstall.sh \
+  README.md \
+  CHANGELOG.md
